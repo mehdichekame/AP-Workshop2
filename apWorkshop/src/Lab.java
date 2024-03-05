d4b5d8545e7b9ec94bd429194f3912abd3a69711
MehdiCh <mehdi.chekame@gmail.com>
2024-03-04 17:43:39 -0800

    Class Lab

--- apWorkshop/src/Lab.java
+++ apWorkshop/src/Lab.java
@@ -1,3 +1,86 @@
 public class Lab {
-    
+    private Student[] students;
+    private String teacherName;
+    private String dayOfWeek;
+    private int maxSize;
+    private int currentSize;
+    private double avgGrde;
+
+    public Lab(String teacherName, String dayOfWeek, int maxSize) {
+        this.teacherName = teacherName;
+        this.dayOfWeek = dayOfWeek;
+        this.maxSize = maxSize;
+        students = new Student[maxSize];
+        currentSize = 0;
+    }
+    // getters
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public String getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public int getCurrentSize() {
+        return currentSize;
+    }
+
+    public double getAvgGrde() {
+        return avgGrde;
+    }
+
+    // setters
+
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public void setDayOfWeek(String dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public void setCurrentSize(int currentSize) {
+        this.currentSize = currentSize;
+    }
+
+    public void setAvgGrde(double avgGrde) {
+        this.avgGrde = avgGrde;
+    }
+
+
+    // methods
+
+    public void calAvg() {
+        double sum = 0;
+        for (int i = 0; i < currentSize; i++) {
+            sum += students[i].getGrade();
+        }
+        avgGrde = sum / currentSize;
+        System.out.println("Avrage Grade of Students: " + avgGrde);
+    }
+
+    public void enrollStudent(Student student) {
+        currentSize++;
+        students[currentSize - 1] = student;
+    }
+
+
+
 }
